013c00b25b5c78bac817833e63d816b94dd50178
Lorenzo Fontoura <lorenzo@nioel.com>
2024-10-25 12:31:48 +1000

    fix: improve system message

--- example/plugin/plugin.ts
+++ example/plugin/plugin.ts
@@ -4,5 +4,9 @@ import {OrderInput} from "./generated/OrderInput";
 export function orderBurger(input: OrderInput): OrderOutput {
   // Estimated delivery of 45 minutes.
   const estimatedDeliveryTimeHours: f64 = 0.75;
-  return new OrderOutput(estimatedDeliveryTimeHours, "success");
+  return new OrderOutput(
+    estimatedDeliveryTimeHours,
+    "the burger order was sent successfully.\n" +
+    `delivery estimated in ${estimatedDeliveryTimeHours*60} minutes`
+  );
 }
